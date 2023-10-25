e1ba0e154b0caf90f9f86ad7fb9d7dbdf2dc05bd
Karelys Guevara <katerineg147@gmail.com>
2023-10-24 23:26:56 -0300

    app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Landing from "./Components/ComponentLanding/Landing";
+import Create from "./Components/ComponentCreate/Create";
+import Home from "./Components/ComponentHome/Home";
+import Details from "./Components/ComponentDetails/Details";
+import React, { useState } from "react";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={"/"} component={Landing} />
+          <Route path={"/create"} component={Create} />
+
+          <Route
+            path="/home"
+            render={(routeProps) => (
+              <Home
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                {...routeProps}
+              />
+            )}
+          />
+          <Route path={"/details/:id"} component={Details} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
